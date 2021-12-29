edaa555234eaf413c087401918081577fd474805
Fernando Perez <ferchofpz@hotmail.com>
2021-12-29 09:42:26 -0500

    Configuracion HTML

--- src/app/componentes/configuracion/configuracion.component.ts
+++ src/app/componentes/configuracion/configuracion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-configuracion',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./configuracion.component.css']
 })
 export class ConfiguracionComponent implements OnInit {
+  permitirRegistro: boolean = false;
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  guardar(){}
 }
